1cec78002dca1b533eb984a5a1de9e164868938a
Andre30062007 <bnblackninja501@gmail.com>
2025-05-30 13:33:03 -0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.de.ecommerce',
+  appName: 'app-de-ecommerce',
+  webDir: 'dist'
+};
+
+export default config;
